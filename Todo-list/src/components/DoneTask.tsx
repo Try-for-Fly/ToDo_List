362e79d52b561f622750f924c2876002c1555fbb
S.A.P.V.Rupasinghe <sapvrupasinghe@gmail.com>
2024-06-16 11:36:14 +0530

    Get done list from backend

--- Todo-list/src/components/DoneTask.tsx
+++ Todo-list/src/components/DoneTask.tsx
@@ -1,8 +1,27 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+
+interface DoneList {
+  id: number;
+  title: string;
+  completed: boolean;
+}
+
 const DoneTask = () => {
+  const [doneList, setDoneList] = useState<DoneList[]>([]);
+
+  useEffect(() => {
+    axios
+      .get<DoneList[]>("http://localhost:9090/todo-service/dones")
+      .then((res) => setDoneList(res.data));
+  }, []);
+
   return (
     <>
       <div className="flex justify-between px-6 py-5 mt-6 bg-[#070814] text-[#78CFB0] rounded-md">
-        <p className="text-center line-through">To study React fundamentals</p>
+        {doneList.map((item) => (
+          <li className="text-center line-through">{item.title}</li>
+        ))}
       </div>
     </>
   );
